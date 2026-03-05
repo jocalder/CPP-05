9d8049c6aa01a52906f4998bc0064ff6b7f9a9c4
Jonathan Calderon Bonilla <jocalder@c2r10s4.42madrid.com>
2026-03-05 13:46:52 +0100

    ex03

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: jocalder <jocalder@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 12:06:39 by jocalder          #+#    #+#             */
-/*   Updated: 2026/03/05 12:58:26 by jocalder         ###   ########.fr       */
+/*   Updated: 2026/03/05 13:46:14 by jocalder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,43 @@ Intern&	Intern::operator=(const Intern& other)
 
 Intern::~Intern() {}
 
-AForm*	Intern::makeForm(const std::string& name, const std::string& target)
-{}
+AForm*	Intern::createShrubbery(const std::string& target)
+{
+	return new ShrubberyCreationForm(target);
+}
+
+AForm*	Intern::createRobotomy(const std::string& target)
+{
+	return new RobotomyRequestForm(target);
+}
+
+AForm*	Intern::createPresidential(const std::string& target)
+{
+	return new PresidentialPardonForm(target);
+}
+
+AForm* Intern::makeForm(const std::string& name, const std::string& target)
+{
+	std::string forms[3] = {
+		"shrubbery creation",
+		"robotomy request",
+		"presidential pardon"
+	};
+
+	AForm* (Intern::*functions[3])(const std::string&) = {
+		&Intern::createShrubbery,
+		&Intern::createRobotomy,
+		&Intern::createPresidential
+	};
+	
+	for (int i = 0; i < 3; i++)
+	{
+		if (name == forms[i])
+		{
+			std::cout << "Intern creates " << name << std::endl;
+			return (this->*functions[i])(target);
+		}
+	}
+	std::cout << "Intern cannot create form: " << name << std::endl;
+	return NULL;
+}
